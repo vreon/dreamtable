504052794b197fee422dd30f6b9adf7535fe5c0c
Jesse Dubay <jesse@jessedubay.com>
2020-08-17 01:43:17 -0700

    finish porting pencil and dropper to ECS

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ class WorldContext:
 
     color_primary: Color = (255, 255, 255, 255)
     color_secondary: Color = (0, 0, 0, 255)
+    color_dropper: Color = (0, 0, 0, 0)
 
     # Just active cameras; kept in sync by a processor
     cameras: Mapping[PositionSpace, Any] = field(default_factory=dict)
@@ -240,6 +241,7 @@ class Canvas:
 class Image:
     image: Any = None
     texture: Any = None
+    image_data: Any = None
     filename: str = None
     dirty: bool = False
 
@@ -578,7 +580,7 @@ class BoxSelectionController(esper.Processor):
                         image=pyray.gen_image_color(
                             int(selection_rect.width),
                             int(selection_rect.height),
-                            draw_tool.color_secondary,
+                            self.world.context.color_secondary,
                         )
                     ),
                     CellGrid(3, 3),
@@ -732,6 +734,8 @@ class ImageController(esper.Processor):
                 if img.image.format != image_format:
                     pyray.image_format(pyray.pointer(img.image), image_format)
 
+                img.image_data = pyray.get_image_data(img.image)
+
                 for ext in self.world.try_component(ent, Extent):
                     ext.width = img.image.width
                     ext.height = img.image.height
@@ -740,7 +744,8 @@ class ImageController(esper.Processor):
                 img.texture = pyray.load_texture_from_image(img.image)
 
             if img.texture and img.dirty:
-                pyray.update_texture(img.texture, pyray.get_image_data(img.image))
+                img.image_data = pyray.get_image_data(img.image)
+                pyray.update_texture(img.texture, img.image_data)
                 img.dirty = False
 
 
@@ -751,6 +756,8 @@ class ImageDeleteController(esper.Processor):
         for _, (img, del_) in self.world.get_components(Image, Deletable):
             if not del_.deleted:
                 continue
+
+            img.image_data = None
 
             if img.texture:
                 pyray.unload_texture(img.texture)
@@ -1011,15 +1018,21 @@ class PencilToolController(esper.Processor):
                 (mouse_pos_x, mouse_pos_y), camera
             )
 
+            mouse_button_down = False
+            if pyray.is_mouse_button_down(pyray.MOUSE_LEFT_BUTTON):
+                mouse_button_down = True
+                color = self.world.context.color_primary
+            elif pyray.is_mouse_button_down(pyray.MOUSE_RIGHT_BUTTON):
+                mouse_button_down = True
+                color = self.world.context.color_secondary
+
             if (
                 (self.drawing or not self.world.context.mouse_reserved)
-                and pyray.is_mouse_button_down(pyray.MOUSE_LEFT_BUTTON)
+                and mouse_button_down
                 and point_rect_intersect(pencil_pos.x, pencil_pos.y, rect)
             ):
                 self.world.context.mouse_reserved = True
                 self.drawing = True
-
-                color = self.world.context.color_primary  # todo
 
                 if self.last_pos_x is None:
                     self.last_pos_x = pencil_pos.x
@@ -1044,11 +1057,84 @@ class PencilToolController(esper.Processor):
                 self.last_pos_y = pencil_pos.y
                 img.dirty = True
 
-            if pyray.is_mouse_button_released(pyray.MOUSE_LEFT_BUTTON):
+            if pyray.is_mouse_button_released(
+                pyray.MOUSE_LEFT_BUTTON
+            ) or pyray.is_mouse_button_released(pyray.MOUSE_RIGHT_BUTTON):
                 self.world.context.mouse_reserved = False
                 self.drawing = False
                 self.last_pos_x = None
                 self.last_pos_y = None
+
+
+class PencilToolRenderer(esper.Processor):
+    def process(self):
+        context = self.world.context
+
+        if context.tool not in (Tool.PENCIL, Tool.DROPPER):
+            return
+
+        mouse_pos_x = context.mouse_pos_x
+        mouse_pos_y = context.mouse_pos_y
+
+        rect = pyray.Rectangle(int(mouse_pos_x) + 16, int(mouse_pos_y) - 16, 16, 16)
+        pyray.draw_rectangle_rec(rect, context.color_primary)
+        pyray.draw_rectangle_lines_ex(get_outline_rect(rect), 1, (255, 255, 255, 255))
+
+        rect = pyray.Rectangle(int(mouse_pos_x) + 33, int(mouse_pos_y) - 16, 16, 16)
+        pyray.draw_rectangle_rec(rect, context.color_secondary)
+        pyray.draw_rectangle_lines_ex(get_outline_rect(rect), 1, (255, 255, 255, 255))
+
+
+class DropperToolController(esper.Processor):
+    def __init__(self):
+        self.color = None
+
+    def process(self):
+        context = self.world.context
+
+        if context.tool != Tool.DROPPER:
+            return
+
+        mouse_pos_x = context.mouse_pos_x
+        mouse_pos_y = context.mouse_pos_y
+
+        context.color_dropper = (0, 0, 0, 0)
+
+        for _, (pos, ext, canvas, img) in self.world.get_components(
+            Position, Extent, Canvas, Image
+        ):
+            camera = self.world.context.cameras[pos.space]
+            rect = pyray.Rectangle(pos.x, pos.y, ext.width, ext.height)
+            dropper_pos = pyray.get_screen_to_world_2d(
+                (mouse_pos_x, mouse_pos_y), camera
+            )
+
+            if point_rect_intersect(dropper_pos.x, dropper_pos.y, rect):
+                x = int(dropper_pos.x - pos.x)
+                y = int(dropper_pos.y - pos.y)
+                context.color_dropper = pyray.get_image_data(img.image)[
+                    y * img.image.width + x
+                ]
+                break
+
+        if pyray.is_mouse_button_pressed(pyray.MOUSE_LEFT_BUTTON):
+            self.world.context.color_primary = context.color_dropper
+        if pyray.is_mouse_button_pressed(pyray.MOUSE_RIGHT_BUTTON):
+            self.world.context.color_secondary = context.color_dropper
+
+
+class DropperToolRenderer(esper.Processor):
+    def process(self):
+        context = self.world.context
+
+        if context.tool != Tool.DROPPER:
+            return
+
+        rect = pyray.Rectangle(
+            int(context.mouse_pos_x) + 16, int(context.mouse_pos_y) - 50, 33, 33
+        )
+        pyray.draw_rectangle_rec(rect, context.color_dropper)
+        pyray.draw_rectangle_lines_ex(get_outline_rect(rect), 1, (255, 255, 255, 255))
 
 
 ################################################################################
@@ -1096,134 +1182,6 @@ def get_outline_rect(rect):
 
 ################################################################################
 # Cruft that needs to be ported to ECS
-
-
-class DrawTool:
-    def __init__(self):
-        self.active = False
-        self.color_primary = (255, 255, 255, 255)
-        self.color_secondary = (0, 0, 0, 255)
-        self.last_pos = None
-
-    def update(self):
-        self.active = pyray.is_key_down(pyray.KEY_X)
-        if not self.active:
-            self.last_pos = None
-
-    def update_thingy(self, thingy, camera_2d):
-        if not thingy:
-            return
-
-        if pyray.is_mouse_button_down(pyray.MOUSE_LEFT_BUTTON):
-            color = self.color_primary
-        elif pyray.is_mouse_button_down(pyray.MOUSE_RIGHT_BUTTON):
-            color = self.color_secondary
-        else:
-            self.last_pos = None
-            return
-
-        mouse_pos = pyray.get_mouse_position()
-        world_pos = pyray.get_screen_to_world_2d(mouse_pos, camera_2d)
-
-        if not self.last_pos:
-            self.last_pos = world_pos
-
-        x1 = int(self.last_pos.x - thingy.x)
-        y1 = int(self.last_pos.y - thingy.y)
-        x2 = int(world_pos.x - thingy.x)
-        y2 = int(world_pos.y - thingy.y)
-        draw_line(
-            x1,
-            y1,
-            x2,
-            y2,
-            lambda x, y: pyray.image_draw_pixel(
-                pyray.pointer(thingy.image), x, y, color
-            ),
-        )
-        self.last_pos = world_pos
-        thingy.dirty = True
-
-    def draw(self):
-        if not self.active:
-            return
-
-        mouse_pos = pyray.get_mouse_position()
-
-        pyray.draw_rectangle(
-            int(mouse_pos.x) + 16, int(mouse_pos.y) - 16, 16, 16, self.color_primary
-        )
-        pyray.draw_rectangle_lines(
-            int(mouse_pos.x) + 15, int(mouse_pos.y) - 17, 18, 18, (255, 255, 255, 255)
-        )
-        pyray.draw_rectangle(
-            int(mouse_pos.x) + 33, int(mouse_pos.y) - 16, 16, 16, self.color_secondary
-        )
-        pyray.draw_rectangle_lines(
-            int(mouse_pos.x) + 32, int(mouse_pos.y) - 17, 18, 18, (255, 255, 255, 255)
-        )
-
-
-class DropperTool:
-    def __init__(self):
-        self.active = False
-        self.color = (0, 0, 0, 0)
-
-    def update(self):
-        self.active = pyray.is_key_down(pyray.KEY_C)
-        if not self.active and self.color:
-            self.color = (0, 0, 0, 0)
-
-    def update_thingy(self, thingy, camera_2d):
-        if thingy:
-            mouse_pos = pyray.get_mouse_position()
-            world_pos = pyray.get_screen_to_world_2d(mouse_pos, camera_2d)
-            x = int(world_pos.x - thingy.x)
-            y = int(world_pos.y - thingy.y)
-            self.color = pyray.get_image_data(thingy.image)[y * thingy.image.width + x]
-        else:
-            self.color = (0, 0, 0, 0)
-
-        if pyray.is_mouse_button_down(pyray.MOUSE_LEFT_BUTTON):
-            draw_tool.color_primary = self.color
-        elif pyray.is_mouse_button_down(pyray.MOUSE_RIGHT_BUTTON):
-            draw_tool.color_secondary = self.color
-
-    def draw(self):
-        if not self.active:
-            return
-
-        mouse_pos = pyray.get_mouse_position()
-
-        # hover color
-        pyray.draw_rectangle(
-            int(mouse_pos.x) + 16, int(mouse_pos.y) - 50, 33, 33, self.color
-        )
-        pyray.draw_rectangle_lines(
-            int(mouse_pos.x) + 15, int(mouse_pos.y) - 51, 35, 35, (255, 255, 255, 255)
-        )
-
-        # todo silly globals
-        pyray.draw_rectangle(
-            int(mouse_pos.x) + 16,
-            int(mouse_pos.y) - 16,
-            16,
-            16,
-            draw_tool.color_primary,
-        )
-        pyray.draw_rectangle_lines(
-            int(mouse_pos.x) + 15, int(mouse_pos.y) - 17, 18, 18, (255, 255, 255, 255)
-        )
-        pyray.draw_rectangle(
-            int(mouse_pos.x) + 33,
-            int(mouse_pos.y) - 16,
-            16,
-            16,
-            draw_tool.color_secondary,
-        )
-        pyray.draw_rectangle_lines(
-            int(mouse_pos.x) + 32, int(mouse_pos.y) - 17, 18, 18, (255, 255, 255, 255)
-        )
 
 
 class CellRefDropperTool:
@@ -1470,9 +1428,7 @@ class GridTool:
 ################################################################################
 
 
-draw_tool = DrawTool()
 grid_tool = GridTool()
-dropper_tool = DropperTool()
 cell_ref_tool = CellRefTool()
 cell_ref_dropper_tool = CellRefDropperTool()
 
@@ -1577,6 +1533,7 @@ def main():
     world.add_processor(MotionController())
     world.add_processor(JitterController())
     world.add_processor(PencilToolController())
+    world.add_processor(DropperToolController())
     world.add_processor(DragController())
     world.add_processor(HoverController())
     world.add_processor(PressController())
@@ -1590,6 +1547,8 @@ def main():
     world.add_processor(CanvasRenderer())
     world.add_processor(DebugEntityRenderer())
     world.add_processor(ButtonRenderer())
+    world.add_processor(DropperToolRenderer())
+    world.add_processor(PencilToolRenderer())
 
     world.add_processor(SelectableDeleteController())
     world.add_processor(ImageDeleteController())
@@ -1599,14 +1558,10 @@ def main():
         pyray.begin_drawing()
         pyray.clear_background(world.context.theme.color_background)
         world.process()
-        # draw_tool.update()
         # grid_tool.update()
-        # dropper_tool.update()
         # cell_ref_tool.update()
         # cell_ref_dropper_tool.update()
-        # draw_tool.draw()
         # grid_tool.draw(camera_2d, theme)
-        # dropper_tool.draw()
         # cell_ref_tool.draw(camera_2d, theme)
         # cell_ref_dropper_tool.draw(camera_2d, theme)
         pyray.end_drawing()
